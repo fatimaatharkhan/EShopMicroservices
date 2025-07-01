0721fc9b5508d65d8bb097a6419a87241fabdef0
Fatima Athar Khan <Fah@cebs.io>
2025-07-01 14:54:13 +0500

    section 5 completed

---
+++ src/Services/Catalog/CatalogAPI/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,33 @@
+﻿using MediatR;
+using System.Windows.Input;
+
+namespace Catalog.API.Products.CreateProduct;
+
+public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
+    : ICommand<CreateProductResult>;
+public record CreateProductResult(Guid Id);
+internal class CreateProductCommandHandler(IDocumentSession session)
+    : ICommandHandler<CreateProductCommand, CreateProductResult>
+{
+    public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+    {
+        //create Product entity from command objects
+
+        var product = new Product
+        {
+            Name = command.Name,
+            Category = command.Category,
+            Description = command.Description,
+            ImageFile = command.ImageFile,
+            Price = command.Price
+        };
+
+        //save to database
+        session.Store(product);
+        await session.SaveChangesAsync(cancellationToken);
+
+        //return CreateProdctResult result
+
+        return new CreateProductResult(product.Id);
+    }
+}
