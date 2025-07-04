14ad01e07e7db75dac183a54357ac849c28d8715
Fatima Athar Khan <Fah@cebs.io>
2025-07-04 17:03:49 +0500

    Section 9 almost completed

---
+++ src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketEndpoint.cs
@@ -0,0 +1,22 @@
+﻿namespace Basket.API.Basket.GetBasket
+{
+    public record GetBasketResponse(ShoppingCart Cart);
+    public class GetBasketEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/basket/{userName}", async (string userName, ISender sender) =>
+            {
+                var result = await sender.Send(new GetBasketQuery(userName));
+
+                var response = result.Adapt<GetBasketResponse>();
+
+                return Results.Ok(response);
+            })
+                .WithName("GetBasket")
+                .WithSummary("Get a user's shopping basket")
+                .Produces<GetBasketResponse>(StatusCodes.Status200OK)
+                .Produces(StatusCodes.Status400BadRequest);
+        }
+    }
+}
