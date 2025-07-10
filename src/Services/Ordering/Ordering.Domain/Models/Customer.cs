d2645e5c20ece7eb14955e266bbf7743ef28c1de
Fatima Athar Khan <Fah@cebs.io>
2025-07-10 18:22:06 +0500

    working on ordering.domain

---
+++ src/Services/Ordering/Ordering.Domain/Models/Customer.cs
@@ -0,0 +1,8 @@
+﻿namespace Ordering.Domain.Models
+{
+    public class Customer : Entity<CustomerId>
+    {
+        public string Name { get; set; } = default!;
+        public string Email { get; set; } = default!;
+    }
+}
